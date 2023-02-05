9e63e2ccd31d34a45ee7b397ac2f975719d32586
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-02-05 17:17:48 -0500

    Remove no longer used functions * Comment out game logic for now * Add updated playRound function * Add DOM elements * Add eventListener for button click

--- foundations_rock_paper_scissors/rps.js
+++ foundations_rock_paper_scissors/rps.js
@@ -1,3 +1,5 @@
+
+
 // Assignment from the Odin Project. https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
 
 function randomIndex(choiceArray) {
@@ -13,27 +15,11 @@ function getComputerChoice() {
     let rps = ["Rock", "Paper", "Scissors"];
     let randomPlay = rps[randomIndex(rps)];
     // prints the selection in console.log
-    console.log(`Computer Selection: ${randomPlay}`);
+//    console.log(`Computer Selection: ${randomPlay}`);
     // returns randomPlay to the function (example: "Scissors")
     return randomPlay;
 }
 
-// this function takes the player input and titleizes it.
-function titleizePlayerSelection(string) {
-    let titleizedSelection = string.charAt(0).toUpperCase() + string.toLowerCase().slice(1);
-    return titleizedSelection;
-}
-
-// consider validating player selection input to only accept items in array
-// function validatePlayerInput(playerSelection) {} // to-do
-
-// this function gets the player input.
-function getPlayerSelection() {
-    let playerSelection = prompt("Please enter 'rock', 'paper', or 'scissors'");
-    // call validatePlayerInput(playerSelection)
-    console.log(`Player selection: ${titleizePlayerSelection(playerSelection)}`);
-    return playerSelection;
-}
 
 function tie(playerSelection, computerSelection) {
     if (computerSelection === "Rock" && playerSelection === "Rock" || 
@@ -65,61 +51,41 @@ function lose(playerSelection, computerSelection) {
     }
 }
 
-// this function takes two params, player selection and computer selection, and it compares them to determine if it's a win, loss, or tie for the player.
-function playRound(playerSelection, computerSelection) {
-    // set fixCasePlayerSelection variable to the return value of titleizePlayerSelection function
-    let titledizedPlayerSelection = titleizePlayerSelection(playerSelection);
-    
-    // this logic is for if the game is a tie
-    if (tie(titledizedPlayerSelection, computerSelection)) {
-        console.log("You tie!");
-        return "tie";
-    }
-    // this is for if the computer wins
-    else if (win(titledizedPlayerSelection, computerSelection)) {
-        console.log(`You lose! ${computerSelection} beats ${titledizedPlayerSelection}.`);
-        return "You lose";
-    }
-    //this is for if the player wins
-    else if (lose(titledizedPlayerSelection, computerSelection)) {
-        console.log(`You win! ${computerSelection} does not beat ${titledizedPlayerSelection}.`);
-        return "You win!";
-    }
-    return 
-}
-
 // this function is the actual game
 function game() {
     // set playerScore & computerScore as initial 0 value variables, meant to calculate score.
     let computerScore = 0;
     let playerScore = 0;
-    // start for loop for 5 rounds of games
-    for (let i = 0; i < 5; i++) {
-        // set playRoundResult variable to the return value from the playRound function
-        let playRoundResult = playRound(getPlayerSelection(), getComputerChoice());
-        // start switch statement and the expression is the result from the playRound function
-        switch (playRoundResult) {
-            // if the result is a "tie" string, then print it's a tie and don't do anything
-            case "tie":
-                console.log("Tied. Do nothing.");
-                break;
-            // if the playRoundResult is "You lose"
-            case "You lose":
-                // increment computerScore by 1
-                computerScore += 1;
-                // print You lost.
-                console.log("You lost.");
-                break;       
-            // if the playRoundResult is "You win!"             
-            case "You win!":
-                // increment playerScore by 1
-                playerScore += 1;
-                console.log("You win!");
-                break;            
-            default:
-                break;
-        }
-    }
+
+    // COMMENTING THIS OUT FOR THE REVISITING RPS EXERCISE // 
+    // LINK HERE: https://www.theodinproject.com/lessons/foundations-revisiting-rock-paper-scissors //
+    // // start for loop for 5 rounds of games
+    // for (let i = 0; i < 5; i++) {
+    //     // set playRoundResult variable to the return value from the playRound function
+    //     let playRoundResult = playRound(getPlayerSelection(), getComputerChoice());
+    //     // start switch statement and the expression is the result from the playRound function
+    //     switch (playRoundResult) {
+    //         // if the result is a "tie" string, then print it's a tie and don't do anything
+    //         case "tie":
+    //             console.log("Tied. Do nothing.");
+    //             break;
+    //         // if the playRoundResult is "You lose"
+    //         case "You lose":
+    //             // increment computerScore by 1
+    //             computerScore += 1;
+    //             // print You lost.
+    //             console.log("You lost.");
+    //             break;       
+    //         // if the playRoundResult is "You win!"             
+    //         case "You win!":
+    //             // increment playerScore by 1
+    //             playerScore += 1;
+    //             console.log("You win!");
+    //             break;            
+    //         default:
+    //             break;
+    //     }
+    // }
     // calculate the winner of all rounds. if the computerScore is greater than playerScore, computer won.
     if (computerScore > playerScore) {
         console.log("Computer won.");
@@ -134,5 +100,47 @@ function game() {
     }
 }
 
+function playRound(playerSelection) {
+    let playerChoice = playerSelection;
+    let computerSelection = getComputerChoice();
+//    console.log(playerChoice);
+
+    if(tie(playerChoice, computerSelection)) {
+        console.log("You tie!");
+        return "tie";
+    }
+    else if (win(playerChoice, computerSelection)) {
+        console.log(`You lose! ${computerSelection} beats ${playerChoice}`);
+        return "You lose";
+    }
+    else if (lose(playerChoice, computerSelection)) {
+        console.log(`You win! ${computerSelection} does not beat ${playerChoice}`);
+        return "You win"
+    }
+    return
+    // console.log(computerSelection);
+}
+
+const displayResults = document.querySelector('#displayResult');
+const playerPick = document.querySelector('#playerChoice');
+const computerPick = document.querySelector('#computerChoice');
+
+const results = document.createElement('div');
+displayResults.appendChild(playerPick);
+displayResults.appendChild(computerPick);
+displayResults.appendChild(results);
+
+
+// rock paper scissors revisited //
+const buttons = document.querySelectorAll('button');
+// loop over all the buttons
+buttons.forEach((button) => {
+        button.addEventListener('click', function(event) {
+            let playerSelection = button.id;
+            let game = playRound(playerSelection);
+            playerPick.textContent = `The player picked: ${playerSelection}`;
+            results.textContent = `The result is: ${game}`;
+        });
+    });
 // invoke function
 game();
