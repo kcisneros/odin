00488fac3ccd2c9886564c565cd8e209f9d9476e
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-02-08 17:01:21 -0500

    Make button work

--- etch-a-sketch/app.js
+++ etch-a-sketch/app.js
@@ -5,26 +5,16 @@ let root = document.querySelector(':root');
 
 let gridSize = 0;
 
-do {
-  gridSize = prompt("Please enter a number between 1 and 100");
-  console.log(`in the if, gridsize is: ${gridSize}`);
-}
-while (gridSize > 100 || gridSize <= 0);
-
-console.log(`before the while, the gridsize is: ${gridSize}`);
-
 function doubleSquare(num) {
   root.style.setProperty('--user-input', gridSize);
 
-  console.log(`in the function, gridsize is: ${gridSize}`);
 
   for (i = 0; i < gridSize; i++) {
     let gridElement = document.createElement('div');
     gridElement.setAttribute("class", "singleGridElement");
     container.appendChild(gridElement);
 
-
-    for (j = 0; j < gridSize -1; j++) {    
+    for (j = 0; j < gridSize; j++) {    
       let square = document.createElement('div');
       square.setAttribute("class", "individualSquare");
       square.style.outline = 'solid';
@@ -39,4 +29,18 @@ function doubleSquare(num) {
   }
 }
 
-doubleSquare();
+const buttons = document.querySelectorAll('#numberPrompt');
+// loop over all the buttons
+buttons.forEach((button) => {
+  container.innerHTML =  '';
+        button.addEventListener('click', function(event) {
+            do {
+              gridSize = prompt("Please enter a number between 1 and 100");
+            }
+            while (gridSize > 100 || gridSize <= 0);
+            // have to clear the container FIRST before then calling the function that makes the grid
+            container.innerHTML =  '';
+            // call function that makes grid
+            doubleSquare();
+        });
+    });
