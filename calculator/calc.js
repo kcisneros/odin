7327c31913f5537c60169e30d8ac114e027434ad
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-02-17 13:29:49 -0500

    * Remove some comments * Add displayValue var * Update CSS to center elements

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,52 +1,26 @@
 // assignment: https://www.theodinproject.com/lessons/foundations-calculator
 
-// my calc has to contain functions for all of the basic math operators
-// start by creating functions for add, subtract, mult, and divide
-
 // let answer = 0;
 
-
-// get existing container div from main html file
 const calcContainer = document.querySelector(".calculatorContainer");
 
-// // create a div called firstSquare
-// const firstSquare = document.createElement('div');
-// // add css class called firstSquare to the created div above
-// firstSquare.classList.add('firstSquare');
-// // add text to the firstSquare div
-// firstSquare.textContent = "1";
-
-// calcContainer.appendChild(firstSquare);
-
 let buttonPressed = '';
+let displayValue = '';
 
 function numButtons() {
-  // try and create 9 squares..
   for (i = 0; i < 10; i++) {
-    // each const will be a button
   const numberButtons = document.createElement('button');
-  // add css class called numberButtons to the created button above
   numberButtons.classList.add('numberButtons');
-  // add text to the numberButtons button. the number is each iteration
-  // the loop
   numberButtons.textContent = `${i}`; 
-  // not sure if i need a dataset rn, maybe just for keyboard clicks?
-  // numberButtons.dataset.key = `${i}`;
-  // add numberButtons to the main container
   calcContainer.appendChild(numberButtons);
-  // add event listener for each button on click
-  // it just alerts which number was pressed. 
   numberButtons.addEventListener('click', () => {
-    alert(`You pressed button number ${numberButtons.textContent}`);
+    // alert(`You pressed button number ${numberButtons.textContent}`);
     buttonPressed = numberButtons.textContent;
     console.log(`in the num buttons function. the button pressed was: ${buttonPressed}`);
     passToDisplay(buttonPressed);
     return buttonPressed;
   });
   };
-  // let buttonPressed = `${numberButtons.textContent}`;
-  // console.log(`the button pressed was: ${buttonPressed}`);
-  // return buttonPressed;
 };
 
 const calcDisplay = document.createElement('div');
@@ -54,18 +28,12 @@ calcDisplay.classList.add('calculatorDisplay');
 calcContainer.appendChild(calcDisplay);
 
 function passToDisplay() {
-
-  // this doesn't work..
-  // // create display that shows number pressed
-  // const calcDisplay = document.createElement('div');
-  // calcDisplay.classList.add('calculatorDisplay')
-  calcDisplay.textContent = `${buttonPressed}`;
-  // calcContainer.appendChild(calcDisplay);
-  console.log(`in the pass to display function.. the num button passed is: ${buttonPressed}`);
+  displayValue = calcDisplay.textContent = `${buttonPressed}`;
+  console.log(`display value is: ${displayValue}`);
+  return displayValue;
 }
 
 function arithmeticButtons() {
-    // create add arithmetic operator button
   const addSymbol = document.createElement('button');
   addSymbol.classList.add('arithmeticOperators');
   addSymbol.textContent = "+";
@@ -74,7 +42,6 @@ function arithmeticButtons() {
     alert(`You pressed the ${addSymbol.textContent} button`);
   });
 
-  // create subtraction arithmetic operator button
   const minusSymbol = document.createElement('button');
   minusSymbol.classList.add('arithmeticOperators');
   minusSymbol.textContent = "-";
@@ -83,7 +50,6 @@ function arithmeticButtons() {
     alert(`You pressed the ${minusSymbol.textContent} button`);
   });
 
-  // create division arithmetic operator button
   const divisionSymbol = document.createElement('button');
   divisionSymbol.classList.add('arithmeticOperators');
   divisionSymbol.textContent = "/";
@@ -92,7 +58,6 @@ function arithmeticButtons() {
     alert(`You pressed the ${divisionSymbol.textContent} button`);
   });
 
-  // create multiplication arithmetic operator button
   const multiplicationSymbol = document.createElement('button');
   multiplicationSymbol.classList.add('arithmeticOperators');
   multiplicationSymbol.textContent = "*";
@@ -117,39 +82,6 @@ function arithmeticButtons() {
     alert(`You pressed the ${clearSymbol.textContent} button`);
   });
 }
-
-
-// // create add arithmetic operator button
-// const addSymbol = document.createElement('button');
-// addSymbol.textContent = "+";
-// calcContainer.appendChild(addSymbol);
-// addSymbol.addEventListener('click', () => {
-//   alert(`You pressed the ${addSymbol.textContent} button`);
-// });
-
-// // create subtraction arithmetic operator button
-// const minusSymbol = document.createElement('button');
-// minusSymbol.textContent = "-";
-// calcContainer.appendChild(minusSymbol);
-// minusSymbol.addEventListener('click', () => {
-//   alert(`You pressed the ${minusSymbol.textContent} button`);
-// });
-
-// // create division arithmetic operator button
-// const divisionSymbol = document.createElement('button');
-// divisionSymbol.textContent = "/";
-// calcContainer.appendChild(divisionSymbol);
-// divisionSymbol.addEventListener('click', () => {
-//   alert(`You pressed the ${divisionSymbol.textContent} button`);
-// });
-
-// // create multiplication arithmetic operator button
-// const multiplicationSymbol = document.createElement('button');
-// multiplicationSymbol.textContent = "*";
-// calcContainer.appendChild(multiplicationSymbol);
-// multiplicationSymbol.addEventListener('click', () => {
-//   alert(`You pressed the ${multiplicationSymbol.textContent} button`);
-// });
 
 // function getFirstNumber, getSecondNumber, and getOperator are to see how it would work in
 // the web browser console
@@ -216,18 +148,6 @@ function operate(firstNum, operator, secondNum) {
     console.log("invalid operator. please enter arithmetic operator");
   }
 }
-
-// testing individ arithmetic funcs
-// add(2, 2);
-// subtract(2, 3);
-// multiply(3, 3);
-// divide(12, 3);
-
-// testing operate function
-// operate("+", 1, 5);
-// operate("/", 10, 5);
-// operate("*", 1, 5);
-// operate("-", 1, 5);
 
 // testing the operate function with functions as parameters
 // getting the user input for first number, operator, and second number.
