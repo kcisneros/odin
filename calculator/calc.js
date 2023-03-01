d44ca53bb6e30a9f3c0c2f06a012e86956670ada
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-03-01 11:16:53 -0500

    * Refactor number button creation & event listener * Add rounding to 2 decimals for operations * Clean up comments & added current issue in comments for self

--- calculator/calc.js
+++ calculator/calc.js
@@ -14,6 +14,8 @@ let equalSymbolClicked = ''
 let operatorClicked = '';
 let secondNumberClicked = '';
 
+let numberButton = '';
+
 let finalOperandValue = '';
 let secondFinalOperandValue = '';
 
@@ -21,87 +23,47 @@ const calcDisplay = document.createElement('div');
 calcDisplay.classList.add('calculatorDisplay');
 calcContainer.appendChild(calcDisplay);
 
-// // const numberButton = document.createElement('button');
-
-// let numberButton = '';
-
-// // create buttons first
-// function numButtons() {
-//   for (i = 0; i < 10; i++) {
-//     numberButton = document.createElement('button');
-//     numberButton.classList.add('numberButton');
-//     numberButton.textContent = `${i}`;
-//     calcContainer.appendChild(numberButton);
-//   }
-//   console.log(`number button is: ${numberButton.length}`);
-
-// }
-
-// // numberButton = document.getElementsByClassName('numberButton');
-
-// // create event listener for buttons
-// function numButtonEventListener() {
-//   numberButton = document.getElementsByClassName('numberButton');
-//   // console.log(`numbner button is ${numberButton}`);
-//   for (let i = 0; i < numberButton.length; i++) {
-//     // console.log(`numbner button is ${numberButton[i]}`);
-//     numberButton[i].addEventListener('click', () => {
-//       if (operatorClicked != "" ) { // operator button has been clicked
-//         // second if? if secondNumberClicked is not empty then add to it, else make it just second number?
-//         secondNumberClicked = Number(numberButton[i].textContent); 
-//         secondFinalOperandValue += secondNumberClicked;
-//         console.log(`the second number value clicked is: ${secondNumberClicked}`);
-//         console.log(`the second operand value clicked is: ${secondFinalOperandValue}`); // shows double digit nums
-//         // console.log(`second number clicked is: ${secondFinalOperandValue}`);
-//         console.log(`number button is ${numberButton[i]}`);
-//         passToDisplay(secondFinalOperandValue); //commenting this line out seemed to clear out the display value issue i was seeing where it was adding the string data type numbers together (aka 21 instead of just 2 and just 1)
-//         // passToDisplay(secondNumberClicked);
-//       }
-//       else { // the operator button hasn't been clicked
-//         firstNumberClicked = numberButton[i].textContent;
-//         finalOperandValue += firstNumberClicked;
-//         console.log(`the first number clicked is: ${firstNumberClicked}`);
-//         console.log(`the first operand clicked is: ${finalOperandValue}`); // shows double digit nums
-//         // console.log(`first number clicked is: ${finalOperandValue}`);
-//         console.log(`number button is ${numberButton[i]}`);
-
-//         passToDisplay(finalOperandValue);
-//       }
-//   });
-//   }
-// }
-
+// create buttons first
 function numButtons() {
   for (i = 0; i < 10; i++) {
-    const numberButton = document.createElement('button');
+    numberButton = document.createElement('button');
     numberButton.classList.add('numberButton');
-    numberButton.textContent = `${i}`; 
+    numberButton.textContent = `${i}`;
     calcContainer.appendChild(numberButton);
-    numberButton.addEventListener('click', () => {
+  }
+  console.log(`number button is: ${numberButton.length}`);
+}
+
+// create event listener for buttons
+function numButtonEventListener() {
+  numberButton = document.getElementsByClassName('numberButton');
+  // console.log(`numbner button is ${numberButton}`);
+  for (let i = 0; i < numberButton.length; i++) {
+    // console.log(`numbner button is ${numberButton[i]}`);
+    numberButton[i].addEventListener('click', () => {
       if (operatorClicked != "" ) { // operator button has been clicked
         // second if? if secondNumberClicked is not empty then add to it, else make it just second number?
-        secondNumberClicked = Number(numberButton.textContent); 
+        secondNumberClicked = numberButton[i].textContent; 
         secondFinalOperandValue += secondNumberClicked;
         console.log(`the second number value clicked is: ${secondNumberClicked}`);
         console.log(`the second operand value clicked is: ${secondFinalOperandValue}`); // shows double digit nums
-
         // console.log(`second number clicked is: ${secondFinalOperandValue}`);
+        console.log(`number button is ${numberButton[i]}`);
         passToDisplay(secondFinalOperandValue); //commenting this line out seemed to clear out the display value issue i was seeing where it was adding the string data type numbers together (aka 21 instead of just 2 and just 1)
         // passToDisplay(secondNumberClicked);
-
       }
       else { // the operator button hasn't been clicked
-        firstNumberClicked = Number(numberButton.textContent);
+        firstNumberClicked = numberButton[i].textContent;
         finalOperandValue += firstNumberClicked;
         console.log(`the first number clicked is: ${firstNumberClicked}`);
         console.log(`the first operand clicked is: ${finalOperandValue}`); // shows double digit nums
         // console.log(`first number clicked is: ${finalOperandValue}`);
+        console.log(`number button is ${numberButton[i]}`);
         passToDisplay(finalOperandValue);
       }
-    });
-  };
-};
-
+  });
+  }
+}
 
 function passToDisplay(number) {
   displayValue = calcDisplay.textContent = number;
@@ -160,9 +122,9 @@ function equalButton() {
     equalSymbolClicked = "=";
     console.log(`the equal operator button is: ${equalSymbolClicked}`);
     operate(finalOperandValue, operatorClicked, secondFinalOperandValue);
-    finalOperandValue = 0;
+    finalOperandValue = '';
     operatorClicked = '';
-    secondFinalOperandValue = 0;
+    secondFinalOperandValue = '';
   });
 }
 
@@ -176,24 +138,28 @@ function clearButton() {
     finalOperandValue = '';
     secondFinalOperandValue = '';
     operatorClicked = '';
+    answer = '';
     console.log(`the displayValue is: ${displayValue}`)
   });
 }
  
 function add(firstNum, secondNum) {
   answer = Number(firstNum) + Number(secondNum); // not sure why I have to declare the number type here again..
+  answer = Math.round(answer * 100) / 100;
   // console.log(`the addition answer is: ${answer}`);
   return answer;
 };
 
 function subtract(firstNum, secondNum) {
   answer = firstNum - secondNum;
+  answer = Math.round(answer * 100) / 100;
   // console.log(`the subtraction answer is: ${answer}`);
   return answer;
 }
 
 function multiply(firstNum, secondNum) {
   answer = firstNum * secondNum;
+  answer = Math.round(answer * 100) / 100;
   // console.log(`the multiplication answer is: ${answer}`);
   // displayValue = answer;
   return answer;
@@ -201,6 +167,7 @@ function multiply(firstNum, secondNum) {
 
 function divide(firstNum, secondNum) {
   answer = firstNum / secondNum;
+  answer = Math.round(answer * 100) / 100;
   // console.log(`the division answer is: ${answer}`);
   return answer;
 }
@@ -229,7 +196,6 @@ function operate(firstNum, arithmeticOperator, secondNum) {
     else {
       firstNum = answer;
       secondNum = 0;
-
       secondNum = secondFinalOperandValue;
       console.log(`firstnum is ${firstNum}`);
       console.log(`secondnum is ${secondNum}`);
@@ -245,14 +211,12 @@ function operate(firstNum, arithmeticOperator, secondNum) {
     else {
       firstNum = answer;
       secondNum = 0;
-
       secondNum = secondFinalOperandValue;
       console.log(`firstnum is ${firstNum}`);
       console.log(`secondnum is ${secondNum}`);
       passToDisplay(add(firstNum, secondNum));  
       console.log(`the math that's happening: ${firstNum} ${arithmeticOperator} ${secondNum}, the answer is ${answer}`); 
     }
-    // 12 + 7 - 5 * 3 =
   }
   else if (arithmeticOperator === "-") {
     if (answer == 0) {
@@ -262,7 +226,6 @@ function operate(firstNum, arithmeticOperator, secondNum) {
     else {
       firstNum = answer;
       secondNum = 0;
-
       secondNum = secondFinalOperandValue;
       console.log(`firstnum is ${firstNum}`);
       console.log(`secondnum is ${secondNum}`);
@@ -277,26 +240,15 @@ function operate(firstNum, arithmeticOperator, secondNum) {
 
 numButtons();
 passToDisplay(0);
-// numButtonEventListener();
+numButtonEventListener();
 arithmeticButtons();
 equalButton();
 clearButton();
 
+// pseudocode can be found here: https://jamboard.google.com/d/19jn6yRp9j1SMViSUJZOhzyi9M6nSDwYPEeXEj7kneQI/viewer?f=0
+// current issue:
+// 1. trying to figure out how to chain operations without pressing the = button
 
-
-// user clicks number 4 ✅
-// number 4 is stored in global variable called firstNumberClicked ✅
-// user clicks "+" button ✅
-// "+" string is stored in global var called operatorClicked ✅
-// user clicks number 3 ✅
-// number 3 is stored in global variable called secondNumberClicked ✅
-// user clicks "=" button ✅
-// operate method is called with (firstNumberClicked, operatorClicked, secondNumberClicked) values ✅
-// then somehow calculator UI displays result of operate method
-
-// change to number. current types are strings ✅
-// fix rest of operators. should be similar to add ✅
-// get clear button to entirely clear everything from the saved state ✅
-// when I type 99 99 shuold appear. when I hit plus it shuold store it. ✅
-// do multi-number operands
-// operator would store number pressed. 
+// notes
+// 1. I think I figured out how to get the buttons function to be separated from the event listener for the numbers
+// 2. will try and replicate that for the operation buttons after committing the current working code
