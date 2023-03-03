402e846508c05526e66f7d006ee6fdf15ac74b8e
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-03-03 10:43:01 -0500

    * Rename functions * Remove comments

--- calculator/calc.js
+++ calculator/calc.js
@@ -33,8 +33,7 @@ const calcDisplay = document.createElement('div');
 calcDisplay.classList.add('calculatorDisplay');
 calcContainer.appendChild(calcDisplay);
 
-// create buttons first
-function numButtons() {
+function createNumButtons() {
   for (i = 0; i < 10; i++) {
     numberButton = document.createElement('button');
     numberButton.classList.add('numberButton');
@@ -43,29 +42,22 @@ function numButtons() {
   }
 }
 
-// create event listener for buttons
-function numButtonEventListener() {
+function addNumButtonEventListeners() {
   numberButton = document.getElementsByClassName('numberButton');
-  // console.log(`numbner button is ${numberButton}`);
   for (let i = 0; i < numberButton.length; i++) {
-    // console.log(`numbner button is ${numberButton[i]}`);
     numberButton[i].addEventListener('click', () => {
-      if (calculatorState.operatorClicked != "" ) { // operator button has been clicked
-        // second if? if secondNumberClicked is not empty then add to it, else make it just second number?
+      if (calculatorState.operatorClicked != "" ) { 
         secondNumberClicked = numberButton[i].textContent; 
         calculatorState.secondFinalOperandValue += secondNumberClicked;
         console.log(`the second number value clicked is: ${secondNumberClicked}`);
-        console.log(`the second operand value clicked is: ${calculatorState.secondFinalOperandValue}`); // shows double digit nums
-        // console.log(`second number clicked is: ${secondFinalOperandValue}`);
-        passToDisplay(calculatorState.secondFinalOperandValue); //commenting this line out seemed to clear out the display value issue i was seeing where it was adding the string data type numbers together (aka 21 instead of just 2 and just 1)
-        // passToDisplay(secondNumberClicked);
+        console.log(`the second operand value clicked is: ${calculatorState.secondFinalOperandValue}`); 
+        passToDisplay(calculatorState.secondFinalOperandValue); 
       }
-      else { // the operator button hasn't been clicked
+      else { 
         firstNumberClicked = numberButton[i].textContent;
         calculatorState.finalOperandValue += firstNumberClicked;
         console.log(`the first number clicked is: ${firstNumberClicked}`);
-        console.log(`the first operand clicked is: ${calculatorState.finalOperandValue}`); // shows double digit nums
-        // console.log(`first number clicked is: ${finalOperandValue}`);
+        console.log(`the first operand clicked is: ${calculatorState.finalOperandValue}`);
         passToDisplay(calculatorState.finalOperandValue);
       }
   });
@@ -79,7 +71,7 @@ function passToDisplay(number) {
   return calculatorState.displayValue;
 }
 
-function arithmeticButtons() {
+function createArithmeticButtons() {
   symbols.forEach((symbolName) => {
     let arithmeticButton = document.createElement('button');
     arithmeticButton.classList.add('arithmeticOperators', symbolName);
@@ -88,10 +80,8 @@ function arithmeticButtons() {
   })
 }
 
-// event listener (click) for arithmetic buttons
-function arithmeticButtonsEventListener() {
+function addArithmeticButtonsEventListeners() {
   symbols.forEach((symbolName) => {
-    // addSymbol (symbol) = document.getElementsByClassName(addSymbol (symbol))
     let buttons = document.getElementsByClassName(symbolName);
     buttons[0].addEventListener('click', () => {
       if (calculatorState.secondFinalOperandValue != '') {
@@ -99,22 +89,20 @@ function arithmeticButtonsEventListener() {
         calculatorState.finalOperandValue = calcDisplay.textContent;
         calculatorState.secondFinalOperandValue = '';
       }
-      console.log(`btnclick is: ${buttons[0].textContent}. operator clicked state is: ${calculatorState.operatorClicked}`);
       calculatorState.operatorClicked = buttons[0].textContent;
       return calculatorState.operatorClicked;
     });
   })
 }
 
-function equalButton() {
+function createEqualButton() {
   equalSymbol = document.createElement('button');
   equalSymbol.classList.add('arithmeticOperators', 'equalSymbol');
   equalSymbol.textContent = "=";
   calcContainer.appendChild(equalSymbol);
 }
 
-// event listener for equal button
-function equalButtonEventListener() {
+function addEqualButtonEventListener() {
   equalSymbol = document.getElementsByClassName('equalSymbol');
   equalSymbol[0].addEventListener('click', () => {
     equalSymbolClicked = equalSymbol[0].textContent;
@@ -126,8 +114,7 @@ function equalButtonEventListener() {
   });
 }
 
-// creates clear button
-function clearButton() {
+function createClearButton() {
   clearSymbol = document.createElement('button');
   clearSymbol.classList.add('arithmeticOperators');
   clearSymbol.classList.add('clearSymbol');
@@ -135,8 +122,7 @@ function clearButton() {
   calcContainer.appendChild(clearSymbol);
 }
 
-// event listener for clear button
-function clearButtonEventListener() {
+function addClearButtonEventListener() {
   clearSymbol = document.getElementsByClassName('clearSymbol');
   clearSymbol[0].addEventListener('click', () => {
     calcDisplay.textContent = 0;
@@ -160,7 +146,6 @@ function add(firstNum, secondNum) {
 function subtract(firstNum, secondNum) {
   calculatorState.answer = firstNum - secondNum;
   calculatorState.answer = Math.round(calculatorState.answer * 100) / 100;
-  // console.log(`the subtraction answer is: ${answer}`);
   return calculatorState.answer;
 }
 
@@ -168,8 +153,6 @@ function subtract(firstNum, secondNum) {
 function multiply(firstNum, secondNum) {
   calculatorState.answer = firstNum * secondNum;
   calculatorState.answer = Math.round(calculatorState.answer * 100) / 100;
-  // console.log(`the multiplication answer is: ${answer}`);
-  // displayValue = answer;
   return calculatorState.answer;
 }
 
@@ -180,7 +163,6 @@ function divide(firstNum, secondNum) {
   }
   calculatorState.answer = firstNum / secondNum;
   calculatorState.answer = Math.round(calculatorState.answer * 100) / 100;
-  // console.log(`the division answer is: ${answer}`);
   return calculatorState.answer;
 }
 
@@ -236,15 +218,15 @@ function operate(firstNum, arithmeticOperator, secondNum) {
   }
 }
 
-numButtons();
+createNumButtons();
 passToDisplay(0);
-numButtonEventListener();
-arithmeticButtons();
-equalButton();
-clearButton();
-arithmeticButtonsEventListener();
-equalButtonEventListener();
-clearButtonEventListener();
+addNumButtonEventListeners();
+createArithmeticButtons();
+createEqualButton();
+createClearButton();
+addArithmeticButtonsEventListeners();
+addEqualButtonEventListener();
+addClearButtonEventListener();
 
 // pseudocode can be found here: https://jamboard.google.com/d/19jn6yRp9j1SMViSUJZOhzyi9M6nSDwYPEeXEj7kneQI/viewer?f=0
 
