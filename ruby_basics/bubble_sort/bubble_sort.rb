f616052a04e17de640d3c246f3686b2204c3342f
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-03-31 16:19:16 -0400

    Finish bubblesort & have sexy tests

--- ruby_basics/bubble_sort/bubble_sort.rb
+++ ruby_basics/bubble_sort/bubble_sort.rb
@@ -4,32 +4,53 @@
 # > bubble_sort([4,3,78,2,0,2])
 # => [0,2,2,3,4,78]
 
-# initial pseudocode:
-# 1. have a loop that compares the element on the left (array[index]),
-# to the element to the right of it (array[index+1]). this loop should run 
-# while the elements are still unsorted
-  
-# 2. if the element to the left is GREATER THAN the element to the right (aka: 4 > 3),
-# swap the positions (save in new array)
-#   2a. else, leave the positions as they are
+# everything just keeps getting more difficult... 
 
-# 3. return the final sorted array?
+def bubble_sort(array)  
+  swapping = true 
 
-def bubble_sort(array)
-  sorted_array = []
-  array.each_with_index do |number, index|
-    p "number is: #{number}"
-    p "index is: #{index}"
-    p "next number is: #{array[index+1]}"
-    if number > array[index+1]
-      sorted_array.push(array[index+1], number)
-      puts "sorted_array is: #{sorted_array}"
-      puts "#{number} is greater than #{array[index+1]}, shift it!"
-    else
-      puts "#{number} is not greater than #{array[index+1]}, don't shift it!"
+  while swapping 
+    swapping = false
+
+    array.each_with_index do |number, index|
+
+      # if the number or the next number are nil (end of array)
+      # immediately stop the code
+      if number.nil? || array[index+1].nil?
+        break   
+      end
+
+      # puts "swapping at the top is: #{swapping}"
+
+      # puts "number is: #{number}"
+      # puts "next num is #{array[index+1]}"
+      # puts "index is: #{index}"
+
+        # if number is greater than the next number
+        if number > array[index+1]
+          # set the swapping var to true (because we're swapping)
+          swapping = true
+          # puts "#{number} is greater than #{array[index+1]}, shift it!"
+          # swap the values in the array. aka 4, 3 becomes 3, 4 in the array
+          array[index], array[index+1] = array[index+1], array[index]
+          # puts "updated array in the if is: #{array}"
+
+        # if the above isn't true
+        else
+          # puts "#{number} is not greater than #{array[index+1]}, don't shift it!"
+          # leave the values in the array as they are. aka 3, 4 stays 3, 4
+          array[index+1], array[index] = array[index+1], array[index]
+          # puts "updated array in the else is: #{array}"
+        end
+        # puts "swapping var is: #{swapping}"
+
+        # puts "array.size+1 is: #{array.size+1}"
+        # puts "array.index+1 is: #{array[index]+1}"
+
+        # puts "swapping at the bottom is: #{swapping}"
     end
   end
-  # return sorted_array
+  return array
 end
 
 bubble_sort([4,3,78,2,0,2])
--- ruby_basics/bubble_sort/spec/bubble_sort_spec.rb
+++ ruby_basics/bubble_sort/spec/bubble_sort_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe 'Bubble Sort Exercises' do
       expect(bubble_sort([4,3,78,2,0,2])).to eq([0,2,2,3,4,78])
     end
 
-    # it 'should return [1, 5, 6, 23, 35, 36, 42, 900]' do
-    #   expect(bubble_sort([23, 5, 1, 6, 900, 35, 36, 42])).to eq([1, 5, 6, 23, 35, 36, 42, 900])
-    # end
+    it 'should return [1, 5, 6, 23, 35, 36, 42, 900]' do
+      expect(bubble_sort([23, 5, 1, 6, 900, 35, 36, 42])).to eq([1, 5, 6, 23, 35, 36, 42, 900])
+    end
 
   end
 end
