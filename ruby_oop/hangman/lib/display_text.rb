f184caf048cb4e3efb94c697353fa921f004406e
kcisneros <16183934+kcisneros@users.noreply.github.com>
2023-05-05 20:15:57 -0400

    * Clean up displayable text module * Add lose text to module * Make some methods in Hangman class private * Remove some puts statements * Refactor win? method * Add method to reveal text if it is in secret word * Rename methods to hopefully better names * Add each_turn method which holds logic for happens during game

--- ruby_oop/hangman/lib/display_text.rb
+++ ruby_oop/hangman/lib/display_text.rb
@@ -3,19 +3,19 @@
 # module that has the words displayed to the screen
 module DisplayableText
   PROMPT_FOR_LETTER = 'Enter a letter to solve the secret word: '
-  GAME_OVER_TEXT = 'Game over! You guessed the word'
-  NO_MATCH_TEXT = 'Not a match'
+  GAME_OVER_TEXT = 'Game over! You guessed the word 🎉'
+  NO_MATCH_TEXT = 'Letter is not in the hangman word!'
   WELCOME_TEXT = "Let's play Hangman!\n\n"
 
   def guessed_letters(guessed_letters)
     puts "\n\nSo far you have guessed: #{guessed_letters}"
   end
 
-  def guess_word_confirmation(guess_word)
-    "guess word is: #{guess_word.join('')}"
+  def turns_left(turn_number)
+    "You have #{11 - turn_number} turns left.\n\n"
   end
 
-  def turns_left(turn_number)
-    "You have #{11 - turn_number} turns left"
+  def lose_text(hangman_word)
+    "Sorry, you lost! Word was: #{hangman_word}"
   end
 end
--- ruby_oop/hangman/lib/hangman.rb
+++ ruby_oop/hangman/lib/hangman.rb
@@ -14,15 +14,19 @@
 # (and their position in the word, e.g. _ r o g r a _ _ i n g) and which incorrect
 # letters have already been chosen. Every turn, allow the player to make a guess
 # of a letter. It should be case insensitive. Update the display to reflect whether
-# the letter was correct or incorrect. If out of guesses, the player should lose. Now
-# implement the functionality where, at the start of any turn, instead of making a
+# the letter was correct or incorrect. If out of guesses, the player should lose. 
+# CHECK! this is done ^^
+
+# Now implement the functionality where, at the start of any turn, instead of making a
 # guess the player should also have the option to save the game. Remember what you
 # learned about serializing objects… you can serialize your game class too! When the
 # program first loads, add in an option that allows you to open one of your saved games,
 # which should jump you exactly back to where you were when you saved. Play on!
+
 require './random_word'
 require './display_text'
 
+# this is the Hangman game class
 class Hangman
   include DisplayableText
 
@@ -33,18 +37,62 @@ class Hangman
     @prompt_guess_letter = ''
   end
 
-  def random_word
-    @hangman_word = @secret_word.random_word.chomp
-  end
-
-# keep playing until game_over? is true
   def play
-    puts random_word
+    random_word
     turn_number = 0
+    puts "#{create_hidden_lines} Word to guess is #{@hangman_word.length} letters long!"
     until game_over?(turn_number)
       each_turn(turn_number)
       turn_number += 1
     end
+  end
+
+  private
+
+  def display_turns_left(turn_number)
+    if turn_number == 11
+      puts lose_text(@hangman_word)
+    else
+      puts turns_left(turn_number)
+    end
+  end
+
+  def win?
+    return true if @hidden_word_lines.eql?(@hangman_word)
+  end
+
+  def reveal_letter_if_it_exists
+    @hangman_word.chars.each_with_index do |letter, index|
+      @hidden_word_lines[index] = letter if letter == @prompt_guess_letter
+    end
+    @hidden_word_lines
+  end
+
+  def check_if_prompt_guess_letter_is_in_hangman_word
+    if @hangman_word.include?(@prompt_guess_letter)
+      @guess_word << @prompt_guess_letter
+    else
+      puts DisplayableText::NO_MATCH_TEXT
+    end
+#    @guess_word
+  end
+
+  def each_turn(turn_number)
+    prompt_for_letter
+    display_guessed_letters
+    check_if_prompt_guess_letter_is_in_hangman_word
+    puts reveal_letter_if_it_exists
+    display_turns_left(turn_number)
+  end
+
+  def display_guessed_letters
+    @guessed_letters << @prompt_guess_letter
+    puts guessed_letters(@guessed_letters)
+  end
+
+  def prompt_for_letter
+    puts DisplayableText::PROMPT_FOR_LETTER
+    @prompt_guess_letter = gets.chomp.downcase
   end
 
   def game_over?(turn_number)
@@ -60,49 +108,12 @@ class Hangman
     @game_over
   end
 
-  def win?
-    if @guess_word.join('').eql?(@hangman_word)
-      true
-    end
+  def create_hidden_lines
+    @hidden_word_lines = '-' * @hangman_word.length
   end
 
-  def prompt_for_letter
-    puts DisplayableText::PROMPT_FOR_LETTER
-    @prompt_guess_letter = gets.chomp.downcase
-  end
-
-  def display_guessed_letters
-    @guessed_letters << @prompt_guess_letter
-    puts guessed_letters(@guessed_letters)
-  end
-
-  def each_turn(turn_number)
-    display_secret_word_length
-    prompt_for_letter
-    display_guessed_letters
-    join_guess_word_into_string_if_there_is_a_match
-    display_turns_left(turn_number)
-  end
-
-  def join_guess_word_into_string_if_there_is_a_match
-    # this puts is only here for testing purposes, will be removed
-    puts "hangman word is: #{@hangman_word}"
-    if @hangman_word.include?(@prompt_guess_letter)
-      @guess_word << @prompt_guess_letter
-      puts guess_word_confirmation(@guess_word)
-      # display_secret_word_length.replace('-', @prompt_guess_letter)
-    else
-      puts DisplayableText::NO_MATCH_TEXT
-    end
-#    @guess_word
-  end
-
-  def display_secret_word_length
-    @hangman_word.length.times { puts '_' }
-  end
-
-  def display_turns_left(turn_number)
-    puts turns_left(turn_number)
+  def random_word
+    @hangman_word = @secret_word.random_word.chomp
   end
 end
 
